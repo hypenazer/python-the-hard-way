74e5a36bd1b3addbf46315bdddf3f43133b7f8f4
Scott Champion <scott@Scotts-MacBook-Air.local>
2016-03-22 22:02:10 -0700

    Completed exercise 1 study drills

--- ex1.py
+++ ex1.py
@@ -5,3 +5,5 @@ print "This is fun."
 print 'Yay! Printing.'
 print "I'd much rather you 'not'."
 print 'I "said" do not touch this.'
+print 'this is on one line\nthis is on a second line'
+#print 'this is a comment'
