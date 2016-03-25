927dffd90ea1cf5f350c4d9db2c997c6eec268df
Scott Champion <champion@uw.edu>
2016-03-25 09:24:12 -0700

    Completed exercise 4 study drills

--- ex4.py
+++ ex4.py
@@ -1,10 +1,19 @@
+# Initialize the day's commuter data
 cars = 100
-space_in_a_car = 4.0
+space_in_a_car = 4		# making this 4.0 will cause line 13 to output 1 decimal place; all other outputs are unchanged
 drivers = 30
 passengers = 90
+
+# set count of cars not driven
 cars_not_driven = cars - drivers
+
+# set count of cars driven
 cars_driven = drivers
+
+# carpool_capacity is total number of seats available including driver seat
 carpool_capacity = cars_driven * space_in_a_car
+
+# set the number of passengers needed in each car to accomodate all passengers.
 average_passengers_per_car = passengers / cars_driven
 
 print "There are", cars, "cars available."
@@ -18,3 +27,5 @@ print "We need to put about", average_passengers_per_car, "in each car."
 #  the error "'car_pool_capacity' is not defined" will be thrown if
 #  the variable is defined as 'carpool_capacity' on line 7 but referenced
 #  as 'car_pool_capacity' on line 13
+
+# 
