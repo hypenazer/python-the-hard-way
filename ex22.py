9626884a19e02c3c7eb9cf312d4e38ef9dbdf7ab
Scott Champion <champion@uw.edu>
2016-04-02 23:36:10 -0700

    Added Python string escape characters

--- ex22.py
+++ ex22.py
@@ -1,1 +1,18 @@
 # This file will contain notes for exercise 22.
+# 
+# Python escape characters:
+# \\ Backslash (\)
+# \' Single-quote (')
+# \" Double-quote (")
+# \a ASCII bell (BEL)
+# \b ASCII backspace (BS)
+# \f ASCII formfeed (FF)
+# \n ASCII linefeed (LF)
+# \N{name} Character named name in the Unicode database
+# \r Carriage return(CR)   <-- I wasn't able to get this to work on osx
+# \t Horizontal Tab (TAB)
+# \uxxx Character with 16-bit hex value xxxx
+# \Uxxxxxxxx Character with 32-bit hex value xxxxxxxx
+# \v ASCII vertical tab (VT)
+# \ooo character with octal value ooo
+# \xhh character with hex value
