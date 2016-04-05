596ca06f331f21339ddab31bf5d12b9f1a75d790
Scott Champion <champion@uw.edu>
2016-04-04 21:16:18 -0700

    Completed exercise 29 study drills

--- ex29.py
+++ ex29.py
@@ -1,6 +1,6 @@
-people = 20
-cats = 30
-dogs = 15
+people = 25
+cats = 10
+dogs = 25
 
 if people < cats:
 	print "Too many cats! The world is doomed!"
@@ -25,3 +25,8 @@ if people <= dogs:
 if people == dogs:
 	print "People are dogs."
 
+# Study Drills
+# 1. if is conditional, code below is executed if condition evaluates as True
+# 2. indent/de-dent is used to indicate the end of the condition
+# 3. de-dented lines of code will run regardless of condition
+# 4. 
