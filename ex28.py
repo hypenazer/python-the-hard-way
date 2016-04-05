f42a2823c4b910dc9ce5cb88a346c4f0b143ea55
Scott Champion <champion@uw.edu>
2016-04-04 21:06:31 -0700

    Completed exercise 28 study drills

---
+++ ex28.py
@@ -0,0 +1,8 @@
+# Python Boolean operators
+!= # not equal
+== # is equal
+>= # greater than or equal
+<= # less than or equal
+<> # not equal; deprecated convention, included for backwards compatibility only
+is # is equal
+is not # not equal
