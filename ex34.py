f41b5a5280096976e3dfc385a6831ad4e583d237
Scott Champion <champion@uw.edu>
2016-04-10 21:10:58 -0700

    Completed exercise 34

---
+++ ex34.py
@@ -0,0 +1,33 @@
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+1. The animal at 1.
+The animal at 1 is the 2nd animal and is a python.
+The 2nd animal is at 1 and is a python.
+
+2. The third (3rd) animal
+The third (3rd) animal is at 2 and is a peacock.
+The animal at 2 is the 3rd animal and is a peacock.
+
+3. The first (1st) animal
+The first (1st) animal is at 0 and is a bear.
+The animal at 0 is the 1st animal and is a bear.
+
+4. The animal at 3.
+The animal at 3 is the 4th animal and is a kangaroo.
+The fourth (4th) animal is at 3 and is a kangaroo.
+
+5. The fifth (5th) animal.
+The fifth (5th) animal is at position 4 and is a whale.
+The animal at 4 is the 5th animal and is a whale.
+
+6. The animal at 2.
+The animal at 2 is the 3rd animal and is a peacock.
+The 3rd animal is at position 2 and is a peacock.
+
+7. The sixth (6th) animal.
+The sixth animal is at position 5 and is a platypus.
+The animal at position 5 is the 6th animal and is a platypus.
+
+8. The animal at 4.
+The animal at 4 is the 5th animal and is a whale.
+The 5th animal is at position 4 and is a whale.
