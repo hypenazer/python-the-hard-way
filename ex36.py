c143748f0ba3a54ff2e1ae64f7f95f72e2190df0
Scott Champion <champion@uw.edu>
2016-04-11 21:01:43 -0700

    Made notes about writing conditionalsn

--- ex36.py
+++ ex36.py
@@ -1,3 +1,4 @@
+# Rules for if-statements
 # 1. Every if-statement must have an else.
 # 2. If this else should never run because it doesn't make sense,
 #	 then you must use a die function in the else that prints out
@@ -8,3 +9,12 @@
 # 	 is like a set of sentences. Put blank lines before and after.
 # 5. Boolean tests should be simple. If they are complex, move their
 #	 calculations to variables earlier in your function.
+
+# Rules for loops
+# 1. Use a while-loop to loop forever, and the probably means never
+# 2. Use a for-loop for all other types of looping
+
+# Tips for debugging
+# 1. Don't use a debugger
+# 2. Use the print command
+# 3. Write small pieces of code at a time 
